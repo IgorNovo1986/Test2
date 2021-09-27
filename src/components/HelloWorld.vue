7eb718ac6feb6f97ae60529ea191c12665e30f21
IgorNovo <igor.novodon@gmail.com>
2021-09-27 16:17:05 +0300

    pars

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,38 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div>
+    <img :src="imgUrl" alt="">
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li
+          v-for="(ingredient, index) in ingredients"
+          :key="index"
+      > {{index + 1}}
+        <span style="margin-left: 20px">{{ingredient.id}}</span>
+        {{ingredient.text}} -- {{ingredient.rank}}</li>
     </ul>
   </div>
 </template>
 
 <script>
+
+import apiService from "../services/api/api.service";
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   },
-  async created() {
-  }
+  data () {
+    return {
+      imgUrl: null,
+      ingredients: []
+    }
+  },
+  async created () {
+      let data = await apiService.get('/product/737628064502.json')
+      this.imgUrl = data.data.product.image_nutrition_small_url
+      console.log(data.data.product)
+      this.ingredients = data.data.product.ingredients
+    }
 }
 </script>
 
@@ -51,8 +46,7 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+text-align: left;
 }
 a {
   color: #42b983;
--- src/services/api/api.service.js
+++ src/services/api/api.service.js
@@ -3,7 +3,7 @@ import axios  from "axios";
 class ApiService {
 
   init() {
-    axios.defaults.baseURL = `https://world.openfoodfacts.org`;
+    axios.defaults.baseURL = `https://world.openfoodfacts.org/api/v0`;
     axios.defaults.headers = {
       "Content-type": "application/json",
     };
