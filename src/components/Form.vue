aa0e25cfae8ae4104adb04f477e43ccb4c756a5b
IgorNovo <igor.novodon@gmail.com>
2021-09-29 23:37:19 +0300

    input

---
+++ src/components/Form.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <form target="_blank" action="">
+              <label for="name">
+              <input id="name" v-for="(name, value, index) in userData" :key="index">{{value}}
+              </label>
+      </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      userData: 
+        {
+         email: '', 
+         firstName: '' ,
+         lastName: '' ,
+         phone:'',
+        }
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+form {
+  display:flex;
+
+}
+
+</style>
